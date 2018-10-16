ded557fac8ad04475d973d1cac136c035dfd24f8
Hazar <hazar.akbuz@outlook.com>
2018-10-16 21:34:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.content === '-new' || message.content === '-oluştur' || message.content === -'olustur') {
 		message.guild.createChannel(`ticket-${message.author}`, "text").then(c => {
-			let role = message.guild.roles.find("name", 501793846355820574);
+			let role = message.guild.roles.find("name", "\@🛡️ Ticket Yetkilisi");
 			let role2 = message.guild.roles.find("name", "@everyone");
 			c.overwritePermissions(role, {
 				SEND_MESSAGES: true,
