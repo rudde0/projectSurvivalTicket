f5f6bd7663a15d689aa9f31df5dce95e7217ada9
Hazar <hazar.akbuz@outlook.com>
2018-10-16 20:52:05 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,104 +1,88 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const db = require("quick.db");
- 
-const serverStats = {
-    ticketCategoryID: '480112833678409728'
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
 }
- 
-client.on('message', async message => {
-    if (message.author.bot) return;
-    if (message.channel.type !== 'text') {
-        let active = await db.fetch(`support_${message.author.id}`);
-        let guild = client.guilds.get(serverStats.process.env.bot_tokeni);
-        let channel, found = true;
-        try {
-            if (active) client.channels.get(active.channelID)
-                .guild;
-        } catch (e) {
-            found = false;
-        }
-        if (!active || !found) {
-            active = {};
-            channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
-            channel.setParent(serverStats.ticketCategoryID)
-            channel.setTopic(`/complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
- 
-            channel.overwritePermissions("501793846355820574", { //Role id (when someone join my server get this role with id <<, i dont know how to change it for @everyone. This will prevent users to see the channel, only admins will see!
-                VIEW_CHANNEL: false,
-                SEND_MESSAGES: false,
-                ADD_REACTIONS: false
-            });
- 
- 
- 
-            let author = message.author;
-            const newChannel = new Discord.RichEmbed()
-                .setColor('RANDOM')
-                .setAuthor(author.tag, author.avatarURL)
-                .setFooter('Support Ticket Created!')
-                .addField('User', author)
-                .addField('ID', author.id)
-            await channel.send(newChannel);
-            const newTicket = new Discord.RichEmbed()
-                .setColor('RANDOM')
-                .setAuthor(`Hello, ${author.username}`, author.avatarURL)
-                .setFooter('Support Ticket Created!')
-            await author.send(newTicket);
-            active.channelID = channel.id;
-            active.targetID = author.id;
-        }
-        channel = client.channels.get(active.channelID);
-        const dm = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
-            .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
-        await message.author.send(dm);
-        if (message.content.startsWith(prefix + 'complete')) return;
-        const embed5 = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setAuthor(message.author.tag, message.author.avatarURL)
-            .setDescription(message.content)
-            .setFooter(`Message Received - ${message.author.tag}`)
-        await channel.send(embed5);
-        db.set(`support_${message.author.id}`, active);
-        db.set(`supportChannel_${channel.id}`, message.author.id);
-        return;
-    }
-    let support = await db.fetch(`supportChannel_${message.channel.id}`);
-    if (support) {
-        support = await db.fetch(`support_${support}`);
-        let supportUser = client.users.get(support.targetID);
-        if (!supportUser) return message.channel.delete();
-        if (message.content.toLowerCase() === '/complete') {
-            const complete = new Discord.RichEmbed()
-                .setColor('RANDOM')
-                .setAuthor(`Hey, ${supportUser.tag}`, supportUser.avatarURL)
-                .setFooter('Ticket Closed -- Nebulous')
-                .setDescription('*Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot.*')
-            supportUser.send(complete);
-            message.channel.delete();
-            db.delete(`support_${support.targetID}`);
-            let inEmbed = new Discord.RichEmbed()
-                .setTitle('Ticket Closed!')
-                .addField('Support User', `${supportUser.tag}`)
-                .addField('Closer', message.author.tag)
-                .setColor('RANDOM')
-            const staffChannel = client.channels.get('480112833678409728'); //Create a log channel and put id here
-            staffChannel.send(inEmbed);
-        }
-        const embed4 = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setAuthor(message.author.tag, message.author.avatarURL)
-            .setFooter(`Message Received - Nebulous`)
-            .setDescription(message.content)
-        client.users.get(support.targetID)
-            .send(embed4);
-        message.delete({
-            timeout: 10000
-        });
-        embed4.setFooter(`Message Sent -- ${supportUser.tag}`)
-            .setDescription(message.content);
-        return message.channel.send(embed4);
-    }
+
+var prefix = "-";
+
+client.on("ready", () => {
+	console.log('ProjectSurvival ticketbot, etkinleştirildi!')
+	client.user.setPresence({ game: { name: 'projectsurvivalmc.com | -yardım', type: 0 } });
 });
+
+client.on("message", (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	if (message.content.toLowerCase().startsWith(prefix + `yardım`)) {
+		const embed = new Discord.RichEmbed()
+		.setTitle(`:mailbox_with_mail: Ticket Sistemi`)
+		.setColor(0xCF40FA)
+		.setDescription(`Hata bildirimleri, oyuncu şikayetleri için burayı kullanabilirsin.`)
+		.addField(`Tickets`, `[${prefix}ticketaç]() > Destek bildirimi açar.\n[${prefix}ticketkapat]() > Açılan desteği kapatır.`)
+		message.channel.send({ embed: embed });
+  	}
+  }
+
+	if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+		message.channel.send(`API zamanlaması test ediliyor`).then(m => {
+		m.edit(`:ping_pong: API zamanlaması, ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API pingim ` + Math.round(client.ping) + `ms.`);
+	});
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `ticketaç`)) {
+	const reason = message.content.split(" ").slice(1).join(" ");
+	if (!message.guild.roles.exists("name", "Destek Ekibi")) return message.channel.send(`Bu Sunucuda '**Destek Ekibi**' rolünü bulamadım bu yüzden ticket açamıyorum \nEğer sunucu sahibisen, Destek Ekibi Rolünü oluşturabilirsin.`);
+	if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`Zaten açık durumda bir ticketin var.`);
+	message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+		let role = message.guild.roles.find("name", "Destek Ekibi");
+		let role2 = message.guild.roles.find("name", "@everyone");
+		c.overwritePermissions(role, {
+			SEND_MESSAGES: true,
+			READ_MESSAGES: true
+		});
+		c.overwritePermissions(role2, {
+			SEND_MESSAGES: false,
+			READ_MESSAGES: false
+		});
+		c.overwritePermissions(message.author, {
+			SEND_MESSAGES: true,
+			READ_MESSAGES: true
+		});
+	message.channel.send(`:white_check_mark: Destek bildirimin başarıyla oluşturuldu, #${c.name}.`);
+	const embed = new Discord.RichEmbed()
+	.setColor(0xCF40FA)
+	.addField(`${message.author.username}!`, `Başarıyla ticket açtın, lütfen yetkilileri etiketleme.`)
+	.setTimestamp();
+	c.send({ embed: embed });
+	message.delete();
+	}).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `ticketkapat`)) {
+	if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`Bu komut sadece kişisel destek odanda kullanılabilir.`);
+
+	message.channel.send(`Destek kanalını kapatmak istediğine eminsen **-kapat** yazman yeterli.`)
+	.then((m) => {
+		message.channel.awaitMessages(response => response.content === '-kapat.', {
+			max: 1,
+			time: 10000,
+			errors: ['time'],
+	})
+	.then((collected) => {
+		message.channel.delete();
+	})
+	.catch(() => {
+		m.edit('Ticket kapatma isteğin zaman aşımına uğradı.').then(m2 => {
+			m2.delete();
+		}, 3000);
+	});
+	});
+}
+
+});
+
+client.login(process.env.bot_tokeni);
