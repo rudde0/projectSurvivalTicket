4c3d8df4f5e52113257630058531c9205a9c4224
Hazar <hazar.akbuz@outlook.com>
2018-10-16 20:45:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,93 +1,104 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+const db = require("quick.db");
+ 
+const serverStats = {
+    ticketCategoryID: '480112833678409728'
 }
-
-var prefix = "v";
-var token = " ";
-
-client.on("ready", () => {
-  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
-  client.user.setGame(`vhelp / vnew | ${client.guilds.size} servers`);
+ 
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type !== 'text') {
+        let active = await db.fetch(`support_${message.author.id}`);
+        let guild = client.guilds.get(serverStats.process.env.bot_tokeni;
+        let channel, found = true;
+        try {
+            if (active) client.channels.get(active.channelID)
+                .guild;
+        } catch (e) {
+            found = false;
+        }
+        if (!active || !found) {
+            active = {};
+            channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
+            channel.setParent(serverStats.ticketCategoryID)
+            channel.setTopic(`/complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
+ 
+            channel.overwritePermissions("501793846355820574", { //Role id (when someone join my server get this role with id <<, i dont know how to change it for @everyone. This will prevent users to see the channel, only admins will see!
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            });
+ 
+ 
+ 
+            let author = message.author;
+            const newChannel = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(author.tag, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+                .addField('User', author)
+                .addField('ID', author.id)
+            await channel.send(newChannel);
+            const newTicket = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(`Hello, ${author.username}`, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+            await author.send(newTicket);
+            active.channelID = channel.id;
+            active.targetID = author.id;
+        }
+        channel = client.channels.get(active.channelID);
+        const dm = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
+            .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
+        await message.author.send(dm);
+        if (message.content.startsWith(prefix + 'complete')) return;
+        const embed5 = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setDescription(message.content)
+            .setFooter(`Message Received - ${message.author.tag}`)
+        await channel.send(embed5);
+        db.set(`support_${message.author.id}`, active);
+        db.set(`supportChannel_${channel.id}`, message.author.id);
+        return;
+    }
+    let support = await db.fetch(`supportChannel_${message.channel.id}`);
+    if (support) {
+        support = await db.fetch(`support_${support}`);
+        let supportUser = client.users.get(support.targetID);
+        if (!supportUser) return message.channel.delete();
+        if (message.content.toLowerCase() === '/complete') {
+            const complete = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(`Hey, ${supportUser.tag}`, supportUser.avatarURL)
+                .setFooter('Ticket Closed -- Nebulous')
+                .setDescription('*Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot.*')
+            supportUser.send(complete);
+            message.channel.delete();
+            db.delete(`support_${support.targetID}`);
+            let inEmbed = new Discord.RichEmbed()
+                .setTitle('Ticket Closed!')
+                .addField('Support User', `${supportUser.tag}`)
+                .addField('Closer', message.author.tag)
+                .setColor('RANDOM')
+            const staffChannel = client.channels.get('480112833678409728'); //Create a log channel and put id here
+            staffChannel.send(inEmbed);
+        }
+        const embed4 = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setFooter(`Message Received - Nebulous`)
+            .setDescription(message.content)
+        client.users.get(support.targetID)
+            .send(embed4);
+        message.delete({
+            timeout: 10000
+        });
+        embed4.setFooter(`Message Sent -- ${supportUser.tag}`)
+            .setDescription(message.content);
+        return message.channel.send(embed4);
+    }
 });
-
-client.on("guildCreate", (guild) => {
-client.user.setGame(`vhelp / vnew | ${client.guilds.size} servers`);
-    guild.owner.user.send(`Hello! I'm Vulnix!\nThanks for adding me to your guild!\n\nView all of my commands with \`vhelp\`.\nLearn more about me with \`vabout\`.\n\n**About:**Vulnix is a simple Discord support ticket bot that aims to provide easy to use mod and support functions for all servers!\n\n\Enjoy! ~Vulnix Teamn\*Need help? Wanna chill?*  Join the Vulnix Discord! https://discord.gg/HqNPFTC`);
-});
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
-    const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: Vulnix Help`)
-    .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
-    message.channel.send({ embed: embed });
-  }
-
-  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
-    message.channel.send(`Hoold on!`).then(m => {
-    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
-    });
-}
-
-if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support Team");
-        let role2 = message.guild.roles.find("name", "@everyone");
-        c.overwritePermissions(role, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        c.overwritePermissions(role2, {
-            SEND_MESSAGES: false,
-            READ_MESSAGES: false
-        });
-        c.overwritePermissions(message.author, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-        const embed = new Discord.RichEmbed()
-        .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
-        .setTimestamp();
-        c.send({ embed: embed });
-    }).catch(console.error);
-}
-if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
-
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === '-confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
-    });
-}
-
-});
-
-client.login(process.env.bot_tokeni);
