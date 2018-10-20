ff472b693ec7c65f53383b3978611a892d20f9c5
Hazar <hazar.akbuz@outlook.com>
2018-10-20 09:23:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
 					name: client.user.username,
 					icon_url: client.user.avatarURL
 				},
-				title: `Ticket oluşturuldu! (<@${message.author.id}>)`,
+				title: `Ticket oluşturuldu! (<@${message.author.username}>)`,
 				url: "http://www.projectsurvivalmc.com",
 				description: "Ticket odasını oluşturdunuz.",
 				fields: [{
