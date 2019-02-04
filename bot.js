897b1e15e81e97bd30835424f1e74a8c07e9543c
Hazar <hazar.akbuz@outlook.com>
2019-02-04 22:06:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const activities = require('./assets/activity');
+const activities = require('./assets/activities');
 
 /*client.on('ready', () => {
 	console.log('ProjectSurvival ticket bot, aktif!')
