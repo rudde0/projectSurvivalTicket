ca7055675b1856d91cbfe9d565cff4b797a2f1c2
Hazar <hazar.akbuz@outlook.com>
2019-09-29 13:09:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,12 +58,12 @@ const events = {
 client.on('guildMemberUpdate', (oldMember, newMember) => {
 	console.log(`${newMember.roles.has(newMember.guild.roles.array())}`);
 	if (newMember.roles.has(newMember.guild.roles.array()) > 17) {
-		if (newMember.roles.size == 1) {
-			newMember.removeRole(`627782465741783050`);
-		} else if (newMember.roles.size > 1) {
-			newMember.addRole(`627782465741783050`);
-		}
-	} else {
+		//if (newMember.roles.size == 1) {
+		//	newMember.removeRole(`627782465741783050`);
+		//} else if (newMember.roles.size > 1) {
+		newMember.addRole(`627782465741783050`);
+		//}
+	} else if (newMember.roles.size == 1) { {
 	  	newMember.removeRole(`627782465741783050`);
 	}
 });
