0a85c13fdda441416bb2b7428fb6895c4286701e
Hazar <hazar.akbuz@outlook.com>
2020-05-24 11:13:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on('raw', async event => {
 	// Announcements
 	if (message.author.id === '159985870458322944' && (message.id === '713838316738576507')) { //MeeBot
 		if (event.t === "MESSAGE_REACTION_ADD") {
-			message.reactions.removeAll().catch(error => console.error('Tepkileri silerken hata oluştu: ', error));
+			message.reactions.remove('713837840257253478').catch(error => console.error('Tepkileri silerken hata oluştu: ', error));
 			message.react('713837840257253478');
 			message.channel.send(`Kayıt isteğiniz alındı, hoş geldiniz!`)
 				.then(msg => {
